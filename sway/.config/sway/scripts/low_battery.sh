e4ac53a9a8d1ee46da7fcde67f77c6f392820a7b
aliso <aliso@github.com>
2025-11-16 18:08:24 +0200

    little tweaks for sway

--- sway/.config/sway/scripts/low_battery.sh
+++ sway/.config/sway/scripts/low_battery.sh
@@ -2,14 +2,15 @@
 
 while [ True ]; do
     up=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)
+
     percentage=$(echo $up | rg -oP 'percentage:\s+\K[0-9]+(?:\.[0-9]+)?(?=%)')
+    percentage=$(($percentage+0))
+
     state=$(echo $up | rg -oP 'state:\s+discharging')
 
-    if [[ $percentage>30 || -z $state ]]; then
-        sleep 800 
-    elif [[ $percentage<15 && -n "$state" ]]; then
-        notify-send --urgency critical "System" "Low battery!"
+    if [[ $percentage<20 && -n "$state" ]]; then
+        notify-send --urgency critical "Low battery!" "$percentage"
     fi
 
-    sleep 60
+    sleep 800 
 done
